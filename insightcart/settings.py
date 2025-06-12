20cc451f38aace1006afd819337d817ac39d4123
raissaputi <124969006+raissaputi@users.noreply.github.com>
2025-06-12 18:01:45 +0700

    Update settings.py

--- insightcart/settings.py
+++ insightcart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6o!*-i4jw9zr9bo6#n8k0_ma3%tsw7r(&2l%+v%fq%5v61if^2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "insightcart-production.up.railway.app"]
 
 
 # Application definition
